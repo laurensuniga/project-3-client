f80215d1aebd0188440ca43bfbb53155175971ef
Lauren Suniga <laurensuniga@Laurens-MacBook-Pro.local>
2021-01-06 17:20:24 -0800

    adds page components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import Footer from './components/Footer';
 import Header from './components/Header';
+
+import HomePage from './pages/HomePage';
+import DashboardPage from './pages/HomePage';
+import SignupPage from './pages/SignupPage';
+import LoginPage from './pages/LoginPage';
+
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
+
+
 
 function App() {
   return (
     <div className="App">
       <Header />
-        <p>Project 3</p>
+        <main>
+          <Switch>
+            <Route exact path="/" render={props => 
+              <HomePage />
+            } />
+            <Route exact path="/dashboard" render={props => 
+              <DashboardPage />
+            } />
+            <Route exact path="/signup" render={props => 
+              <SignupPage />
+            } />
+            <Route exact path="/login" render={props => 
+              <LoginPage />
+            } />
+          </Switch>
+        </main>
       <Footer />
     </div>
   );
