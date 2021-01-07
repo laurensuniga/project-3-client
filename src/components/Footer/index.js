986532fa37c5b9b5fba97981ea0d010c7e807a68
Lauren Suniga <laurensuniga@Laurens-MacBook-Pro.local>
2021-01-06 16:52:12 -0800

    formats header and footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,10 @@
+import styles from './Footer.module.css';
+
 function Footer(props) {
-    return <footer>Footer</footer>;
+    return (
+        <footer className={styles.Footer}>
+            <p>Copyright &copy; All rights reserved {new Date().getFullYear()} Project 3</p>
+        </footer>);
 };
 
 
