5c07446ffe40f7ebb25f7f719b9a8601ce94bdf3
Lauren Suniga <laurensuniga@Laurens-MacBook-Pro.local>
2021-01-06 18:24:14 -0800

    configs routes props

--- src/App.js
+++ src/App.js
@@ -25,10 +25,10 @@ function App() {
               <DashboardPage />
             } />
             <Route exact path="/signup" render={props => 
-              <SignupPage />
+              <SignupPage {...props} />
             } />
             <Route exact path="/login" render={props => 
-              <LoginPage />
+              <LoginPage {...props}/>
             } />
           </Switch>
         </main>
--- src/pages/SignupPage/index.js
+++ src/pages/SignupPage/index.js
@@ -1,7 +1,62 @@
+import { useState } from 'react';
+
 function SignupPage(props) {
+    const [formState, setFormState] = useState(getInitialFormState());
+
+    function getInitialFormState() {
+        return {
+            firstName: "",
+            lastName: "",
+            email: "",
+            password: "",
+        }
+    }
+    
+
+    function handleChange(event) {
+        setFormState(prevState => ({
+            ...prevState,
+            [event.target.name]: event.target.value
+        }));
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log('submitted form data: ', formState)
+        setFormState(getInitialFormState());
+
+        props.history.push('/dahsboard');
+    }
+
     return (
-        <div>
-            SignupPage
+        <div className="Page">
+            <form onSubmit={handleSubmit}>
+                <input 
+                value={formState.firstName} 
+                onChange={handleChange} 
+                name="firstName" 
+                type="text" 
+                />
+                <input 
+                value={formState.lastName} 
+                onChange={handleChange} 
+                name="lastName" 
+                type="text" 
+                />
+                <input 
+                value={formState.email} 
+                onChange={handleChange} 
+                name="email" 
+                type="email" 
+                />
+                <input 
+                value={formState.password} 
+                onChange={handleChange} 
+                name="password" 
+                type="password" 
+                />
+                <button>Sign Up</button>
+            </form>
         </div>
     );
 }
