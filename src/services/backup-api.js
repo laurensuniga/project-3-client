3b62c3f5a6ca7f36f64be8068bc8a6fdac005b57
Lauren Suniga <laurensuniga@Laurens-MBP.hsd1.ca.comcast.net>
2021-01-25 14:13:16 -0800

    prepares for deployment

--- src/services/backup-api.js
+++
@@ -1,5 +0,0 @@
-// const BASE_URL = 'https://api.adviceslip.com/advice';
-
-// export function getAnimals() {
-//     return fetch(BASE_URL).then(res => res.json());
-// }
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,5 +1,6 @@
 import { setToken, getUserFromToken, removeToken } from './tokenService';
-const BASE_URL = 'http://localhost:3001/api/users';
+const BASE_URL = 'https://finding-furry-friends-api.herokuapp.com/api/users';
+// 'http://localhost:3001/api/users';
 
 
 function signup(user) {
